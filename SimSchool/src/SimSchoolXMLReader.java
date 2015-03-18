c4a13253af102c210e6e977424c7886c0040d396
dkamas273 <dkamas273@PR28.hhs.win.fuhsd.org>
2015-03-18 14:21:32 -0700

    commit 9

--- SimSchool/src/SimSchoolXMLReader.java
+++ SimSchool/src/SimSchoolXMLReader.java
@@ -1,5 +1,63 @@
 
 public class SimSchoolXMLReader {
 	
+	String data;
+	FileIO reader;
 	
+	public SimSchoolXMLReader(String filename) {
+		reader = new FileIO();
+		data = reader.readFile(filename);
+	}
+	
+	public void setFilename(String filename) {
+		data = reader.readFile(filename);
+	}
+	
+	private String returnBetweenTag(String keyword) {
+		return data.substring(data.indexOf('<' + keyword + '>') + keyword.length() + 2, data.indexOf("</" + keyword + '>'));
+	}
+	
+	private String getFirstKeyword() {
+		
+	}
+	
+	private String substringInside(int start) {
+		
+	}
+	
+	public Person[] toPeople() {
+		String k = getFirstKeyword();
+		if (k.equalsIgnoreCase("person")) {
+			
+		}
+	}
+	
+	private Person toPerson(String s) {
+		int age = Integer.parseInt(returnBetweenTag("age"));
+		String gender = returnBetweenTag("gender");
+		String name = returnBetweenTag("name");
+		return new Person(name, age, gender);
+		
+	}
+	
+	private Student toStudent(String s) {
+		Person p = toPerson(s);
+		String ID = returnBetweenTag("ID");
+		double GPA = Double.parseDouble(returnBetweenTag("GPA"));
+		return new Student(p.getName(), p.getAge(), p.getGender(), ID, GPA);
+	}
+	
+	private CollegeStudent toCollegeStudent(String s) {
+		Student p = toStudent(s);
+		String major = returnBetweenTag("major");
+		int year = Integer.parseInt(returnBetweenTag("year"));
+		return new CollegeStudent(p.getName(), p.getAge(), p.getGender(), p.getIdNum(), p.getGPA(), year, major);
+	}
+
+	private Teacher toTeacher(String s) {
+		Person p = toPerson(s);
+		String subject = returnBetweenTag("subject");
+		double salary = Double.parseDouble(returnBetweenTag("salary"));
+		return new Teacher(p.getName(), p.getAge(), p.getGender(), subject, salary);
+	}
 }
